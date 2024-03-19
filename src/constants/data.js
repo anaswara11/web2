4d081311c80f70a379e239f6ecde3390892d169f
anaswara11 <anaswarasuren11@gmail.com>
2024-03-19 17:34:17 +0530

    Initial commit

---
+++ src/constants/data.js
@@ -0,0 +1,190 @@
+import images from './images';
+
+const Menu = [
+    {
+        text: 'About us',
+        link: '/About',
+        // Adding the white text class to "About us"
+        className: 'white-text'
+    },
+    {
+        text: 'Services',
+        link: '#services',
+        // Adding the white text class to "Services"
+        className: 'white-text'
+    },
+    {
+        text: 'Use Cases',
+        link: '#use-cases',
+        className: 'white-text'
+    },
+    {
+        text: 'Process',
+        link: '#process',
+        className: 'white-text'
+    },
+    {
+        text: 'Testimonials',
+        link: '#testimonials',
+        className: 'white-text'
+    },
+  
+];
+
+const ServicesData = [
+    {
+        titleone: 'App Development  ',
+        titletwo: 'Services',
+        link: '/Appdev',
+        itemclass: 'blight',
+        imgURL: images.services01,
+    },
+    {
+        titleone: 'Web Development',
+        titletwo: 'Services',
+        link: '/Webdev',
+        itemclass: 'bgreen',
+        imgURL: images.services02,
+    },
+    {
+        titleone: 'Cyber security',
+        titletwo: 'Services',
+        link: '/Cyber',
+        itemclass: 'bdark',
+        imgURL: images.services03,
+    },
+    {
+        titleone: 'Cloud',
+        titletwo: 'Services',
+        link: '/Cloud',
+        itemclass: 'blight',
+        imgURL: images.services04,
+    },
+    {
+        titleone: 'AI and Machine Learning',
+        titletwo: 'Solutions',
+        link: '/Aiml',
+        itemclass: 'bgreen',
+        imgURL: images.services05,
+    },
+    {
+        titleone: 'Blockchain Development',
+        titletwo: 'Solutions',
+        link: '/Blockchain',
+        itemclass: 'bdark',
+        imgURL: images.services06,
+    },
+]
+const CaseStudies = [
+    {
+        text: "Background In the bustling streets of Silicon Valley, Lisa, a passionate entrepreneur, found herself at acrossroads. Armed with an innovative idea for a health-tech startup, she poured her heart and soul into building her dream venture. However, as she delved deeper into the complexities of product development and market strategy, doubts began to creep in. Faced with technical challenges, funding hurdles, and fierce competition, Lisa’s once-bright vision began to dim.",
+        link: '#',
+    },
+    {
+        text: "The Challenge: Lisa’s story mirrors the struggles of many aspiring entrepreneurs – a dream overshadowed by uncertainty and fear. Despite her unwavering determination, she found herself grappling with the daunting reality of turning her vision into a reality. Feeling overwhelmed and discouraged, Lisa knew she needed a guiding hand to navigate the turbulent waters of entrepreneurship.",
+        link: '#',
+    },
+    {
+        text: "The Solution: Enter Code 69 – a beacon of hope in Lisa’s darkest hour. Upon discovering Code 69’s reputation for providing comprehensive support to startups, Lisa reached out for assistance. From the moment she connected with the Code 69 team, she felt a sense of reassurance and support. With empathy and understanding, the Code 69 experts listened to Lisa’s story, empathized with her challenges, and offered a tailored solution to help her overcom obstacles",
+        link: '#',
+    },
+];
+
+const WorkingProcess = [
+    {
+        title: 'Consultation',
+        description: 'During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.',
+    },
+    {
+        title: 'Research and Strategy Development',
+        description: 'During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.',
+    },
+    {
+        title: 'Implementation',
+        description: 'During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.',
+    },
+    {
+        title: 'Monitoring and Optimization',
+        description: 'During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.',
+    },
+    {
+        title: 'Reporting and Communication',
+        description: 'During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.',
+    },
+    {
+        title: 'Continual Improvement',
+        description: 'During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.',
+    },
+
+];
+
+const Team = [
+    {
+        name: 'Ms. Harshitha Challa',
+        position: 'Founder',
+        info: "A visionary leader with a passion for empowering startups, Harshitha spearheads Code 69's mission, leveraging her strategic acumen to propel the company towards global recognition and success.",
+        foto: images.team01,
+        linkedin: '#',
+    },
+    {
+        name: 'Mr. Bibin S',
+        position: 'CEO',
+        info: "As the driving force behind Code 69, Bibin S combines visionary leadership with tactical expertise to steer the company towards its ambitious goals, ensuring excellence in service delivery and client satisfaction.",
+        foto: images.team02,
+        linkedin: '#',
+    },
+    {
+        name: 'Mr. Anaswer Ajay',
+        position: 'COO',
+        info: "An operational virtuoso, Anaswer Ajay oversees Code 69's day-to-day operations, optimizing efficiency, and logistics to ensure seamless service delivery and client satisfaction.",
+        foto: images.team03,
+        linkedin: 'https://www.linkedin.com/in/anaswerajay/',
+    },
+    {
+        name: 'Aadarsh T R',
+        position: 'CTO',
+        info: "With a wealth of technical expertise and innovation, Adarsh leads Code 69's technical team, guiding the development of cutting-edge solutions that meet the diverse needs of startups, while staying ahead of industry trends and advancements.",
+        foto: images.team04,
+        linkedin: 'https://www.linkedin.com/in/aadarsh-t-r/',
+    },
+    {
+        name: 'David',
+        position: 'Technical Team ',
+        info: "As a skilled web developer, David is instrumental in creating immersive and user-friendly online experiences for our clients. His creativity and proficiency in front-end and back-end technologies enable us to deliver websites that captivate audiences and drive engagement.",
+        foto: images.team05,
+        linkedin: '#',
+    },
+    {
+        name: 'Vyshnav ',
+        position: 'Technical Team ',
+        info: "Vyshnav's expertise in data analytics and machine learning empowers our clients to unlock valuable insights from their data. His proficiency in advanced analytics techniques and predictive modeling enables us to provide data-driven recommendations that drive informed decision-making and business growth.",
+        foto: images.team06,
+        linkedin: '#',
+    },
+
+];
+
+const Testimonials = [
+    {
+        name: 'John Smith',
+        position: 'Marketing Director at XYZ Corp',
+        testimonial: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+    },
+    {
+        name: 'John Smith',
+        position: 'Marketing Director at XYZ Corp',
+        testimonial: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+    },
+    {
+        name: 'John Smith',
+        position: 'Marketing Director at XYZ Corp',
+        testimonial: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+    },
+    {
+        name: 'John Smith',
+        position: 'Tech team',
+        testimonial: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
+    },
+];
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { Menu, CaseStudies, WorkingProcess, Team, Testimonials, ServicesData };
